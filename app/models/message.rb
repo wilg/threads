9064d4b460755c09363112d71069cf307a5f2189
Wil Gieseler <supapuerco@gmail.com>
2012-12-12 21:54:27 -0800

    stupid bugz

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,13 +4,13 @@ class Message < ActiveRecord::Base
   belongs_to :thread, class_name: "MessageThread", touch: true
   has_one :project, through: :thread
 
-  validates_presence_of :thread
+  validates_presence_of :thread, :thread_id
   validates_presence_of :body
 
   # For creating a message without a thread.
-  attr_accessor :project_id, :thread_name
+  attr_accessor :project_id, :thread_name_to_create
 
-  attr_accessible :project_id, :thread_name, :body, :thread_id
+  attr_accessible :project_id, :thread_name_to_create, :body, :thread_id
 
   scope :latest_first, -> { order("created_at desc") }
 
@@ -18,8 +18,8 @@ class Message < ActiveRecord::Base
 
   before_validation :create_thread_if_none_present
   def create_thread_if_none_present
-    if !thread
-      self.thread = Project.find(project_id).threads.create(name: thread_name)
+    if !thread && thread_name_to_create && project_id
+      self.thread = Project.find(project_id).threads.create(name: thread_name_to_create)
     end
   end
 
